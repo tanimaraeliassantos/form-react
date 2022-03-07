21b5c9d8e2c98d1a478c6a17458e647fc50d47a8
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-07 16:07:46 +1100

    Update App.js
    import css to be used
    
    create State to be used by different inputs from form
    Add names of variables
    Logging on console the data from initializing state
    Return different inputs for the form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import React, { useState } from "react";
+import "./App.css";
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comments: "",
+  });
 
-  console.log(firstName);
-
+  console.log(formData);
   function handleChange(event) {
-    setFirstName(event.target.value);
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
   }
+
   return (
     <form>
-      <input type="text" placeholder="First Name" onChange={handleChange} />
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+        value={formData.lastName}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={handleChange}
+        name="email"
+        value={formData.email}
+      />
+      <textarea
+        value={formData.comments}
+        placeholder="Comments"
+        onChange={handleChange}
+        name="comments"
+      />
     </form>
   );
 }
